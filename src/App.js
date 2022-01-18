bc35013350b83e80ed42ffbf071ead699ae74fe9
Dylan <dylanbowmandsk@gmail.com>
2022-01-18 20:49:39 +0000

    added map component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar"
+import Map from "./Map"
 import "./style.css"
-
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-
+      <Map/>
 
     </div>
   );
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,16 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 const Map = () => {
+
     return (
-        <div id="map"></div>
+        <div id="map">
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
     )
 }
 
