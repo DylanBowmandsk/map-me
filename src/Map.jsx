6573382f8a02b3cd6c2a45faddfdd4481c1af133
Dylan <dylanbowmandsk@gmail.com>
2022-01-21 16:35:14 +0000

    map now centers at users position

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,10 +1,21 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useEffect, useState } from 'react'
+import { MapContainer, TileLayer, useMap} from 'react-leaflet'
 
+function MyComponent() {
+    const map = useMap()
+    navigator.geolocation.getCurrentPosition(position => {
+        map.setView([position.coords.latitude, position.coords.longitude])
+    })
+    
+    return null
+  }
 const Map = () => {
 
     return (
         <div id="map">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            
+            <MapContainer center={[	51.509865, 	-0.118092]} zoom={14} scrollWheelZoom={false}>
+                <MyComponent/>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -13,5 +24,7 @@ const Map = () => {
         </div>
     )
 }
+
+
 
 export default Map
