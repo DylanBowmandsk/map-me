9dfca9b557f60d6dd406b170bac87c82d2ef6afc
Dylan <dylanbowmandsk@gmail.com>
2022-01-21 17:40:48 +0000

    marker placement working

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react'
-import { MapContainer, TileLayer, useMap} from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
 
 function MyComponent() {
     const map = useMap()
     navigator.geolocation.getCurrentPosition(position => {
-        map.setView([position.coords.latitude, position.coords.longitude])
+        map.flyTo([position.coords.latitude, position.coords.longitude])
     })
     
     return null
   }
 const Map = () => {
+
+    var markers = [[51.509865,-0.118092],[51.559865,-0.11892]]
 
     return (
         <div id="map">
@@ -20,7 +22,17 @@ const Map = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                {markers.map((x, key) => {
+                    return (
+                        <div key={key}>
+                            <Marker position={x}>
+
+                            </Marker>
+                        </div>
+                    )
+            })}
             </MapContainer>
+
         </div>
     )
 }
